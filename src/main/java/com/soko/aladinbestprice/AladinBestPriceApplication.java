0ccee021710219caa1474b8ffbb5bf119420ada1
JerryK026 <kskyung0624@gmail.com>
2023-09-11 09:31:53 +0900

    first commit

---
+++ src/main/java/com/soko/aladinbestprice/AladinBestPriceApplication.java
@@ -0,0 +1,13 @@
+package com.soko.aladinbestprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AladinBestPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AladinBestPriceApplication.class, args);
+	}
+
+}
