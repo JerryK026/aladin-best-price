c1d9b1b10527aab149ba1ddbe00f611a799cb4bf
JerryK026 <kskyung0624@gmail.com>
2023-09-11 14:31:14 +0900

    feat : Books에 물품을 추가할 때 한 판매자가 여러 책을 올릴 경우 가장 저렴한 것만 추가하도록 하는 기능을 구현한다

--- src/main/java/com/soko/aladinbestprice/Books.java
+++ src/main/java/com/soko/aladinbestprice/Books.java
@@ -2,19 +2,30 @@ package com.soko.aladinbestprice;
 
 import java.util.*;
 
+/**
+ * 같은 종류의 중고책들이 들어간다
+ */
 public class Books implements Iterable<Book> {
     List<Book> books = new ArrayList<>();
 
-    public void add(Book book) {
-        books.add(book);
+    public void add(Book newBook) {
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+
+            if (doesHaveSameSeller(newBook, book)) {
+                if (book.getPrice() <= newBook.getPrice()) return;
+
+                books.remove(i);
+                books.add(newBook);
+                return;
+            }
+        }
+
+        books.add(newBook);
     }
 
     public void add(String title, int price, String seller, int deliveryFare) {
-        books.add(new Book(title, price, new Seller(seller, deliveryFare)));
-    }
-
-    public void add(Books books) {
-        this.books.addAll(books.asList());
+        add(new Book(title, price, new Seller(seller, deliveryFare)));
     }
 
     public int size() {
@@ -34,4 +45,7 @@ public class Books implements Iterable<Book> {
         return this.books.iterator();
     }
 
+    private boolean doesHaveSameSeller(Book newBook, Book book) {
+        return book.getSeller().getName().equals(newBook.getSeller().getName());
+    }
 }
